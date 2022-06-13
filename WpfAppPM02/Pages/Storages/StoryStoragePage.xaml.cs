0eaa66a1867ceba10ac66617ee59fb76ca06afce
Тимурка Тимурка <xajrullin-02@mail.ru>
2022-06-13 20:06:34 +0300

    story storage

--- WpfAppPM02/Pages/Storages/StoryStoragePage.xaml.cs
+++ WpfAppPM02/Pages/Storages/StoryStoragePage.xaml.cs
@@ -12,19 +12,23 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfAppPM02.Pages.Admin;
+using WpfAppPM02.DataBase;
 
 namespace WpfAppPM02.Pages.Storages
 {
-   
-    public partial class StorageManagerPage : Page
+    /// <summary>
+    /// Логика взаимодействия для StoryStoragePage.xaml
+    /// </summary>
+    public partial class StoryStoragePage : Page
     {
-        public StorageManagerPage()
+        public StoryStoragePage()
         {
             InitializeComponent();
-            TabItemFrame1.Navigate(new StoragePage());
-            TabItemFrame2.Navigate(new MaterialPageList());
-            TabItemFrame3.Navigate(new StoryStoragePage());
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+           StoryDataGrid.ItemsSource = MyModel.GetContext().MaterialList.ToList();
         }
     }
 }
